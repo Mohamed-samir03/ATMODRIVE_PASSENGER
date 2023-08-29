48dc693b128254504f8e90830f3c5a5f455fdd6c
mo samir <smyr03130@gmail.com>
2023-08-30 02:51:54 +0300

    finished clean architecture for auth passenger

--- app/src/main/java/com/mosamir/atmodrivepassenger/futures/splash/MainActivity.kt
+++ app/src/main/java/com/mosamir/atmodrivepassenger/futures/splash/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 delay(3000)

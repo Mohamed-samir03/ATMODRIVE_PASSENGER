7b82f9f6f8fd16cb93df1a9da2e9fe039f17ac6d
mo samir <smyr03130@gmail.com>
2023-09-11 18:13:22 +0300

    finished get my location

--- app/src/main/java/com/mosamir/atmodrivepassenger/futures/home/MapsActivity.kt
+++ app/src/main/java/com/mosamir/atmodrivepassenger/futures/home/MapsActivity.kt
@@ -1,21 +1,44 @@
 package com.mosamir.atmodrivepassenger.futures.home
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsResponse
+import com.google.android.gms.location.LocationSettingsStatusCodes
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 import com.mosamir.atmodrivepassenger.R
 import com.mosamir.atmodrivepassenger.databinding.ActivityMapsBinding
+import com.mosamir.atmodrivepassenger.util.showToast
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    var mLocationRequest:LocationRequest ?= null
+    var mLocationCallback:LocationCallback ?= null
+    var mFusedLocationClient:FusedLocationProviderClient ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,23 +50,133 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        initLocation()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private fun initLocation(){
+        if (mFusedLocationClient == null){
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        }
+
+        if (mLocationRequest == null){
+            mLocationRequest = LocationRequest.create()
+                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                .setInterval(2000)
+                .setFastestInterval(2000)
+                .setSmallestDisplacement(5f)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation(){
+
+        mMap.isMyLocationEnabled = true
+        mLocationCallback = object : LocationCallback(){
+            override fun onLocationResult(result: LocationResult) {
+                super.onLocationResult(result)
+
+                moveCameraMap(LatLng(result.lastLocation!!.latitude,result.lastLocation!!.longitude))
+
+            }
+        }
+
+        mFusedLocationClient?.requestLocationUpdates(mLocationRequest!!,mLocationCallback!!,Looper.getMainLooper())
+
+    }
+
+    private fun moveCameraMap(latLng: LatLng){
+        val cameraPosition = CameraPosition.builder().target(latLng).zoom(15f).build()
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
+
+    private fun locationChecker(){
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(mLocationRequest!!)
+
+        val result : Task<LocationSettingsResponse> = LocationServices.getSettingsClient(this)
+            .checkLocationSettings(builder.build())
+
+        result.addOnCompleteListener {task ->
+
+            try {
+                task.getResult(ApiException::class.java)
+                getLocation()
+            }catch (exception : ApiException){
+                when (exception.statusCode){
+
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED ->{
+                        try {
+                            val resolve = exception as ResolvableApiException
+                            startIntentSenderForResult(resolve.resolution.intentSender,
+                            Priority.PRIORITY_HIGH_ACCURACY,null,0,0,0,null)
+                        }catch (ex:Exception){}
+                    }
+
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE ->{
+                        showToast("GPS Not available")
+                    }
+
+                }
+            }
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            Priority.PRIORITY_HIGH_ACCURACY ->{
+                when(resultCode){
+                    Activity.RESULT_OK ->{
+                        getLocation()
+                    }
+                    Activity.RESULT_CANCELED ->{
+                        locationChecker()
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun checkPermission(){
+        if ((ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
+        != PackageManager.PERMISSION_GRANTED) && (
+        ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) ){
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION),2)
+        }else{
+            locationChecker()
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+       mMap.uiSettings.apply {
+           isCompassEnabled = true
+           isZoomControlsEnabled = true
+           isMyLocationButtonEnabled = true
+       }
+        checkPermission()
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 2 && permissions.isNotEmpty() &&
+            grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            locationChecker()
+        }
+
+    }
+
 }

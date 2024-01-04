7eea955c152342d4bdf2f7a21a83841aa3492b6c
mo samir <smyr03130@gmail.com>
2024-01-04 11:27:32 +0200

    add new feature 'setting'

---
+++ app/src/main/java/com/mosamir/atmodrivepassenger/features/setting/presentation/common/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.mosamir.atmodrivepassenger.features.setting.presentation.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mosamir.atmodrivepassenger.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}

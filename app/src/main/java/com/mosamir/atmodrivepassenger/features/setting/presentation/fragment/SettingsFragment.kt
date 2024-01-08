75a63c6f741c2d77c61a2f6746a87eaf1c7cfeb1
mo samir <smyr03130@gmail.com>
2024-01-08 20:28:11 +0200

    finished design setting screens

--- app/src/main/java/com/mosamir/atmodrivepassenger/features/setting/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/com/mosamir/atmodrivepassenger/features/setting/presentation/fragment/SettingsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.mosamir.atmodrivepassenger.R
 import com.mosamir.atmodrivepassenger.databinding.FragmentSettingsBinding
 
@@ -30,7 +31,10 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        binding.backFromSetting.setOnClickListener {
+            val action = SettingsFragmentDirections.actionSettingsFragmentToPassengerProfileFragment()
+            findNavController().navigate(action)
+        }
 
     }
 
